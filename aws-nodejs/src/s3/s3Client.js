e804c7b4b0db0c98fe08d8f2ced99c359b273c5a
balatamilmani <btamilma.ptr@gmail.com>
2021-04-19 21:31:08 -0400

    S3 Bucket creation NodeJs code

--- aws-nodejs/src/s3/s3Client.js
+++ aws-nodejs/src/s3/s3Client.js
@@ -42,21 +42,65 @@ const deleteObject = async (bucketName, objectName, objectVersion) => {
         Bucket: bucketName,
         Key: objectName
     };
-    if(objectVersion){
+    if (objectVersion) {
         params['VersionId'] = objectVersion;
     }
     const response = await s3.deleteObject(params).promise();
     return response;
 }
 
+/**
+ * 
+ * @param {*} bucketName Name of the Bucket to be created
+ * @param {*} locationConstraint 
+ * @returns Bucket creation response
+ */
+const createS3Bucket = async (bucketName, locationConstraint) => {
+    var params = {
+        Bucket: bucketName,
+        ACL: 'private'
+    };
+    //Specifies the Region where the bucket will be created
+    //Default is us-east-1, the value can't be default value
+    if(locationConstraint !== 'us-east-1'){
+        params['CreateBucketConfiguration'] = {'LocationConstraint' : locationConstraint};
+    }
+    let response = await s3.createBucket(params).promise();
+    return response;
+}
+
+/**
+ * 
+ * @param {*} bucketName Name of the Bucket for enabling versioning
+ * @returns putBucketVersioning method of S3 is not returning anything
+ */
+const enableBucketVersioning = async (bucketName) => {
+    var params = {
+        Bucket: bucketName,
+        VersioningConfiguration: {
+            Status: "Enabled"
+        }
+    };
+    let response = await s3.putBucketVersioning(params).promise();
+    return response;
+}
 //Invoke functions using IIFE
 (async () => {
+    let locationConstraint = 'us-east-1';//Default region is us-east-1
     let fileName = 'abcd.txt';
-    let bucketName = 'balaversiontest';
+    let bucketName = 'bbalaversiontest-bucket';
     let versionId = '8wM.btrNW8_UePX1G4nDIgrtokCWSTWW';
+    let response;
     try {
-        let response = await uploadObject(bucketName, fileName);
-        //let response = await deleteObject(bucketName, fileName, versionId);
+        //Create Bucket
+        response = await createS3Bucket(bucketName, locationConstraint);
+        //Enable versioning
+        //response = await enableBucketVersioning(bucketName);
+        //Upload Object
+        //response = await uploadObject(bucketName, fileName);
+        //Deletes particular version of Object if versionId is passed, 
+        //otherwise creates a new Object and marks it as deleted
+        //response = await deleteObject(bucketName, fileName, versionId);
         console.log(response);
     } catch (err) {
         console.error(`err:: ${err}\n, ${response}`);
